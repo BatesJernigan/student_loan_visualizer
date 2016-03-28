06d51df12a81cd1ca1720ef88f9099b175dd9cbc
Cliff <enkentaylor@yahoo.com>
2016-03-28 18:52:01 -0400

    Cliff's function comments
    I added a section for the functions I use in my features. As long as we
    all are mindful of not editing our sections, we should prevent a lot of
    issues.

--- SERVER.R
+++ SERVER.R
@@ -2,6 +2,8 @@ library(shiny)
 library(ggplot2)
 
 server <- shinyServer(function(input, output) {
+
+  #----------------------------------Cliff's Functions------------------------------------------------------------------
   output$histPlot1 <- renderPlot({
     conIn <- file(paste0(paste0("data/StateDebt/StateAverageDebt",input$Year), ".csv"),"r")
     tbl <- read.csv(conIn)
@@ -10,10 +12,13 @@ server <- shinyServer(function(input, output) {
     #hist(tbl$AverageDebt, freq = T, breaks = 20, main = paste("Histogram of",
     #" Average State Debt"), xlab = "Average Debt", ylab = "Number of States", col = "Tan",
     #border = "black", ylim = c(0, 15), xlim = c(10000, 400000))
-    ggplot(data=tbl, aes(tbl$AverageDebt)) + 
+    ggplot(data=tbl, aes(tbl$AverageDebt)) +
       geom_histogram(breaks=seq(10000, 40000, by =500), col="red", aes(fill=..count..))})
+
   output$table1 <- renderDataTable(
     data.frame(read.csv(paste0(paste0("data/StateDebt/StateAverageDebt",input$Year), ".csv"))))
+
   output$table2 <- renderTable(
     read.csv(paste0(paste0("data/StateDebt/StateAverageDebt",input$Year), "Analysis.csv")), include.rownames=FALSE)
-})
+  #---------------------------------------------------------------------------------------------------------------------
+  })
