e5abd94abcb34401bea997a7539acd278e99fedc
Cliff <enkentaylor@yahoo.com>
2016-03-29 18:29:17 -0400

    fixed labels on histogram

--- server.R
+++ server.R
@@ -8,11 +8,12 @@ server <- shinyServer(function(input, output) {
     tbl <- read.csv(conIn)
     tbl_df <- data.frame(tbl)
     close(conIn)
-    #hist(tbl$AverageDebt, freq = T, breaks = 20, main = paste("Histogram of",
-    #" Average State Debt"), xlab = "Average Debt", ylab = "Number of States", col = "Tan",
-    #border = "black", ylim = c(0, 15), xlim = c(10000, 400000))
-    ggplot(data=tbl, aes(tbl$AverageDebt)) +
-      geom_histogram(breaks=seq(10000, 40000, by =500), col="red", aes(fill=..count..))
+    ggplot(data=tbl, aes(tbl$AverageDebt), xlab = "Average State Debt") + 
+      geom_histogram(breaks=seq(10000, 40000, by =500), 
+                     col="red", 
+                     aes(fill=..count..))+ 
+      labs(title="Histogram for Average Debt by State") +
+      labs(x="Average Debt", y="Number of States")
   })
 
   output$table1 <- renderDataTable(
